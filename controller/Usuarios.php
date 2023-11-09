58a1fbbc615c3139fcd8a84901d2f398f0d4a364
agmethjimenez <127366559+agmethjimenez@users.noreply.github.com>
2023-11-08 21:19:25 -0500

    Update Usuarios.php

--- controller/Usuarios.php
+++ controller/Usuarios.php
@@ -1,23 +1,31 @@
-<?php
+
+?><?php
 header('Content-Type: application/json');
-  require_once("../config/conexion.php");
-  require_once("../models/consignacion.php");
-  $consignacion = new Usuarios();
-  
+require_once("../config/conexion.php");
+require_once("../models/consignacion.php");
+$consignacion = new Usuarios();
+
 $body = json_decode(file_get_contents("php://input"), true);
 
-  switch ($_GET["op"]) {
-    case 'GetAll':
-        $datos = $consignacion->getconsignacion();
-        echo json_encode($datos);
-        break;
-    
-    case 'GetID':
-        $datos = $consignacion->getconsignacion_x_id($body['ID']);
-        echo json_encode($datos);
-        break;
-    case 'Insert':
-        $datos = $consignacion->postusuarios($body['ID'],$body['nombre'],$body['apellido'],$body['numero'],$body['direccion']);
-        echo '!Insertado';
-  }
-?>
+switch ($_GET["op"]) {
+  case 'GetAll':
+    $datos = $consignacion->getconsignacion();
+    echo json_encode($datos);
+    break;
+  case 'GetID':
+    $datos = $consignacion->getconsignacion_x_id($body['ID']);
+    echo json_encode($datos);
+    break;
+  case 'Insert':
+    $datos = $consignacion->postusuarios($body['ID'], $body['nombre'], $body['apellido'], $body['numero'], $body['direccion']);
+    echo '!Insertado';
+    break;
+  case 'PUT':
+    $datos = $consignacion->putUsers($body['ID'], $body['nombre'], $body['apellido'], $body['numero'], $body['direccion']);
+    echo '!Actualizado';
+    break;
+  case 'DELETE':
+    $datos = $consignacion->deleteUsers($body['ID']);
+    echo "Borrado";
+    break;
+}
